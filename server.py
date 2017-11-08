b32760ddd20fdd353c5f5da46a2b180ae27f7ed4
makeabignews <makeabignews@sina.com>
2017-11-08 16:55:05 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3 
 # -*- coding:UTF-8 -*-  
 import time 
-import os,sys,system
+import os,sys
 while True:
   time.sleep(5)
-  system('sudo python %s.py' %(os.path.dirname(os.path.realpath(__file__)) ))
+  os.system('sudo python %s.py' %(os.path.dirname(os.path.realpath(__file__)) ))
   time.sleep(55)
